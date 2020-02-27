6ed47876ed30bd02db88fe28dcb3077d56658a05
kyle molidor <k_molidor@yahoo.com>
2020-02-27 04:22:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-  def initialize (name)
+  def initialize (name, breed = "Mut")
     @name = name
+    @breed = breed
   end
   
   def name (name)
@@ -9,6 +10,14 @@ class Dog
   
   def name
     @name
+  end
+  
+  def breed (breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
   end
 end
     
